f5ffb0a11aaf5d220ffd989e5fb5897fe80c3d87
Arthur Cattaneo Zavadski <aczavads@gmail.com>
2014-03-08 11:06:31 -0300

    Adicionado exemplo e teste de Singleton com Universo.java, e o exemplo da eventual necessidade de mudança para contemplar 4 universos.

---
+++ PosUnipar/test/aula20130308/singleton/p2/testes/TesteDoUniverso.java
@@ -0,0 +1,41 @@
+package aula20130308.singleton.p2.testes;
+
+import aula20130308.singleton.p2.Universo;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Arthur
+ */
+public class TesteDoUniverso {
+    
+    public TesteDoUniverso() {
+    }
+    
+    @Test
+    public void testarSeUnviversoEhSingleton() {
+        //Universo u1 = new Universo();
+        //Universo u2 = new Universo();
+        Universo u1 = Universo.criarUniverso();
+        Universo u2 = Universo.criarUniverso();
+        assertSame(
+                "As referências para Universo deveriam"
+                + " apontar para a mesma instância!",
+                u1, u2);
+    }
+    @Test
+    public void testarSeUnviversoEhSingletonUsandoSet() {
+        Set<Universo> universos = new HashSet<Universo>();
+        for (int i = 0; i < 100; i++) {
+            universos.add( Universo.criarUniverso() );
+        }
+        assertEquals("Só deveria existir UM!", 1, universos.size());
+    }
+}
